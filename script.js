82e1a43c06bdaa0e3154fd6f1568f35098e8b56d
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-08-11 16:32:23 -0300

    link add

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ var horas = document.getElementById('horas')
 var minutos = document.getElementById('minutos')
 var segundos = document.getElementById('segundos')
 
-//funçao tempo que pega as horas, minutos, segundos
 var relogio = setInterval(function time() {
     var data = new Date()
-    var hrs = data.getHours() //pega horas
-    var min = data.getMinutes() //pega minutos
-    var seg = data.getSeconds() //pega segundos
+    var hrs = data.getHours() 
+    var min = data.getMinutes() 
+    var seg = data.getSeconds()
 
     horas.textContent = fixTime(hrs)
     minutos.textContent = fixTime(min)
     segundos.textContent = fixTime(seg)
-    //textContent para mudar o texto
 
     function fixTime(time){
-        return time < 10 ? '0' + time : time
+        return time < 10 ? '0'+time : time
     }
-
 })
